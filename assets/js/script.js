051803994d9dde9480656cb8c88e965ad07e0c23
victor segovia <segovia_victor@hotmail.com>
2023-04-27 16:54:45 -0400

    cambio script commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,22 +4,22 @@ $(document).ready(function(){
 
         // Asegúrese de que this.hash tenga un valor antes de anular el comportamiento predeterminado
         if (this.hash !== "") {
-        // Evitar el comportamiento de clic de anclaje predeterminado
-        event.preventDefault();
+            // Evitar el comportamiento de clic de anclaje predeterminado
+            event.preventDefault();
 
-        // Almacenar hash
-        var hash = this.hash;
+            // Almacenar hash
+            var prueba = this.hash;
 
-        // Usando el método animate() de jQuery para agregar desplazamiento de página suave
-         // El número opcional (800) especifica el número de milisegundos que se tarda en desplazarse al área especificada
-        $('html, body').animate({
-            scrollTop: $(hash).offset().top
-        }, 800, function(){
+            // Usando el método animate() de jQuery para agregar desplazamiento de página suave
+            // El número opcional (800) especifica el número de milisegundos que se tarda en desplazarse al área especificada
+            $('html, body').animate({
+                scrollTop: $(prueba).offset().top
+            }, 800, function(){
     
-            // Agregue hash (#) a la URL cuando termine de desplazarse (comportamiento de clic predeterminado)
-            window.location.hash = hash;
-        });
+                // Agregue hash (#) a la URL cuando termine de desplazarse (comportamiento de clic predeterminado)
+                window.location.hash = prueba;
+            });
         } // End if
         // Script extraido desde w3school
     });
-    });
+});
